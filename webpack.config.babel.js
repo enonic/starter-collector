7212e1a3db82ff8281e7ec3dba7824dff57deea6
Christian Westgaard <cwe@enonic.com>
2019-06-26 13:15:25 +0200

    Lib-util-2.1.0, terser and upgrade node modules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 import path from 'path';
+import TerserPlugin from 'terser-webpack-plugin';
 import {webpackEsmAssets} from '@enonic/webpack-esm-assets';
 import {webpackServerSideJs} from '@enonic/webpack-server-side-js';
 
@@ -11,22 +12,37 @@ const WEBPACK_CONFIG = [
 		__dirname,
 		externals: [
 			/^\/lib\/http-client.*$/,
-			/^\/lib\/util.*$/,
-			/^\/lib\/xp\/admin.*$/,
-			/^\/lib\/xp\/auth.*$/,
-			/^\/lib\/xp\/common.*$/,
-			/^\/lib\/xp\/context.*$/,
-			/^\/lib\/xp\/node.*$/,
-			/^\/lib\/xp\/task.*$/,
-			/^\/lib\/xp\/repo.*$/,
-			/^\/lib\/xp\/value.*$/
+
+			'/lib/util',
+			/^\/lib\/util\//,
+
+			/^\/lib\/xp\//,
+			//^\/lib\/xp\/admin.*$/,
+			//^\/lib\/xp\/auth.*$/,
+			//^\/lib\/xp\/common.*$/,
+			//^\/lib\/xp\/context.*$/,
+			//^\/lib\/xp\/node.*$/,
+			//^\/lib\/xp\/task.*$/,
+			//^\/lib\/xp\/repo.*$/,
+			//^\/lib\/xp\/value.*$/
 		],
 		serverSideFiles: [
 			'src/main/resources/main',
 			'src/main/resources/tasks/collect/collect'
 		],
 		mode: MODE,
+		optimization: {
+			minimizer: [
+				new TerserPlugin({
+					terserOptions: {
+						compress: {},
+						mangle: true // Note `mangle.properties` is `false` by default.
+					}
+				})
+			]
+		},
 		resolveAlias: {
+			//'/lib/util': path.resolve(__dirname, '../lib-util/src/main/resources/lib/util'),
 			'/lib/explorer/collector': path.resolve(__dirname, '../lib-explorer-collector/src/main/resources/lib/explorer/collector/'),
 			'/lib/explorer': path.resolve(__dirname, '../lib-explorer/src/main/resources/lib/explorer/')
 		}
